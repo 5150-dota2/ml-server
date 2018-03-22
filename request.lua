dfd8e9175899ec0d3ebf5c24fe234921e4040b82
Steven Than <steventhan11@gmail.com>
2018-03-21 20:19:16 -0400

    update request.lua

--- request.lua
+++ request.lua
@@ -1,11 +1,19 @@
 local dkjson = require( "game/dkjson" )
+local utils = require(GetScriptDirectory() .. "/utils")
 
 local request = {}
 
-function request:Log(item)
+function request:Send(data, callback)
+  local retVal
   request = CreateHTTPRequest(":8080")
-  request:SetHTTPRequestRawPostBody("application/json", dkjson.encode(GetCursorLocation()))
-  -- request:Send(function(response) end)
+  request:SetHTTPRequestRawPostBody('application/json', dkjson.encode(data))
+  request:Send(function(res)
+    if res.StatusCode ~= 200 then
+      print("POST failed")
+      return
+    end
+    callback(dkjson.decode(res.Body))
+   end)
 end
 
 return request;
