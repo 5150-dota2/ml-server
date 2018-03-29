f870bfc8124155a912941076dbd5a4fabbc6dc00
Steven Than <steventhan11@gmail.com>
2018-03-29 05:07:00 -0400

    brain.py now supports both sides

--- brain.py
+++ brain.py
@@ -4,6 +4,8 @@ from torch import optim
 from torch.nn import functional as F
 from torch.autograd import Variable
 import random
+from datetime import datetime
+import os
 
 
 class NeuralNet(nn.Module):
@@ -43,41 +45,65 @@ class ExpReplay(object):
         return len(self.mem)
 
 
-
 class DeepQNet(object):
-    def __init__(self, num_features, num_actions, gamma):
+    def __init__(self, num_features, num_actions, gamma, path=None):
         self.net = NeuralNet(num_features, num_actions)
+        self.opt = optim.Adam(self.net.parameters(), lr=0.001)
+
+        # Load checkpoint from disk
+        if path and os.path.isfile(path):
+            print(self)
+            try:
+                ckpt = torch.load(path)
+                self.net.load_state_dict(ckpt['net_state'])
+                self.opt.load_state_dict(ckpt['opt_state'])
+                print("Checkpoint at {} loaded...".format(path))
+            except Exception as e:
+                print(e)
+                print("Cannot load checkpoint...")
+
         self.gamma = gamma
         self.exp = ExpReplay(100000)
-        self.opt = optim.Adam(self.net.parameters(), lr=0.001)
-        self.last_state = torch.Tensor(num_features).unsqueeze(0)
-        self.last_action = 0
-        self.last_reward = 0
+        self.last_state = {
+            2: torch.Tensor(num_features).unsqueeze(0),
+            3: torch.Tensor(num_features).unsqueeze(0)
+        }
+        self.last_action = {2: 0, 3: 0}
+        self.last_reward = {2: 0, 3: 0}
 
-    def update(self, reward, signal):
-        current_state = torch.Tensor(signal).float().unsqueeze(0)
+    def update(self, reward, current_signal):
+        team = current_signal[0]
+        current_state = torch.Tensor(current_signal).float().unsqueeze(0)
         self.exp.push(
-            (self.last_state, current_state, torch.LongTensor([int(self.last_action)]), torch.Tensor([self.last_reward])))
+            (self.last_state[team], current_state,
+            torch.LongTensor([int(self.last_action[team])]), torch.Tensor([self.last_reward[team]])))
         action = self.select_action(current_state)
-        self.learn()
-        self.last_action = action
-        self.last_state = current_state
-        self.last_reward = reward
+        self.learn(100)
+        self.last_action[team] = action
+        self.last_state[team] = current_state
+        self.last_reward[team] = reward
         return action
 
     def select_action(self, state):
         out = self.net(Variable(state, volatile=True))
-        probs = F.softmax(out*10)
+        probs = F.softmax(out*100)
         return probs.multinomial().data[0, 0]
 
-    def learn(self):
-        if len(self.exp) < 10:
+    def learn(self, batch_size):
+        if len(self.exp) < batch_size:
             return
-        batch_state, batch_next_state, batch_action, batch_reward = self.exp.sample(10)
-        outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
-        next_outputs = self.model(batch_next_state).detach().max(1)[0]
+        batch_state, batch_next_state, batch_action, batch_reward = self.exp.sample(batch_size)
+        outputs = self.net(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
+        next_outputs = self.net(batch_next_state).detach().max(1)[0]
         target = self.gamma * next_outputs + batch_reward
         td_loss = F.smooth_l1_loss(outputs, target)
         self.opt.zero_grad()
         td_loss.backward(retain_variables=True)
         self.opt.step()
+
+
+    def save(self):
+        torch.save({"net_state": self.net.state_dict(),
+                    "opt_state" : self.opt.state_dict(),
+                   }, datetime.now().strftime("ckpt %Y-%m-%d %H.%M.%S.brain"))
+        print("Checkpoint saved.")
